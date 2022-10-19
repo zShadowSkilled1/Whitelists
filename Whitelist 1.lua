92a11f5e1b3187ad4d182ece9e366e2f4f26b9d6
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-10-19 15:45:41 +0200

    Update Whitelist 1.lua

--- Whitelist 1.lua
+++ Whitelist 1.lua
@@ -2,4 +2,6 @@ if _G.SHubEnabled == true then
 if game.Players.LocalPlayer.Name == "Roblox" then
   game.Players.LocalPlayer:Kick("You are blacklisted from Shadow's Hub.")
  end
+  elseif _G.SHubEnabled == false then
+  game.Players.LocalPlayer:Kick("Shadow's Hub Detected A Unnatural Client Behavor.")
   end
