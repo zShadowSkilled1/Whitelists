0ccd76e2b3a951a7f1fb0cc8d08ba807283e14fc
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-10-19 13:17:24 +0200

    Update Whitelist 1.lua

--- Whitelist 1.lua
+++ Whitelist 1.lua
@@ -1,4 +1,4 @@
 
-if game.Players.LocalPlayer.Name = "EDHRKFHEFKEGFKEHFKEF" then
+if game.Players.LocalPlayer.Name == "EDHRKFHEFKEGFKEHFKEF" then
   game.Players.LocalPlayer:Kick("You are blacklisted from Shadow's Hub.")
  end
